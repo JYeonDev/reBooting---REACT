09f4d40d2ec958b4ba8bc9b3348d7c8ba8f5236b
JYeonDev <whddus4837@gmail.com>
2022-05-08 22:48:35 +0900

    Add habit EventListner

--- template/src/components/habit.jsx
+++ template/src/components/habit.jsx
@@ -1,36 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Habit extends Component {
-  state = {
-    count: 0,
-  }
-
   handleIncrement = () => {
-    this.setState({
-      count: this.state.count + 1,
-    })
-  }
+    this.props.onIncrement(this.props.habit);
+  };
 
   handleDecrement = () => {
-    const count = this.state.count -1;
-    this.setState({
-      count: count < 0 ? 0 : count,
-    })
-  }
+    this.props.onDecrement(this.props.habit);
+  };
+
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit);
+  };
   render() {
     const { name, count } = this.props.habit;
     return (
-      <li className="habit">
-        <span className="habit-name">{name}</span>
-        <span className="habit-count">{count}</span>
-        <button className="habit-button habit-increase" onClick={this.handleIncrement}>
-          <i className="fa-solid fa-plus"></i>
+      <li className='habit'>
+        <span className='habit-name'>{name}</span>
+        <span className='habit-count'>{count}</span>
+        <button
+          className='habit-button habit-increase'
+          onClick={this.handleIncrement}
+        >
+          <i className='fa-solid fa-plus'></i>
         </button>
-        <button className="habit-button habit-decrease" onClick={this.handleDecrement}>
-          <i className="fa-solid fa-minus"></i>
+        <button
+          className='habit-button habit-decrease'
+          onClick={this.handleDecrement}
+        >
+          <i className='fa-solid fa-minus'></i>
         </button>
-        <button className="habit-button habit-delete">
-          <i className="fa-solid fa-trash-can"></i>
+        <button
+          className='habit-button habit-delete'
+          onClick={this.handleDelete}
+        >
+          <i className='fa-solid fa-trash-can'></i>
         </button>
       </li>
     );
